26d49af244b66399469bde511d9b234ec466b4e9
StasDondow <125673216+StasDondow@users.noreply.github.com>
2023-10-31 13:13:01 +0200

    SQL task (Easy)

---
+++ 183_Customers_Who_Never_Order.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/customers-who-never-order/description/
+
+SELECT Customers.name AS Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.id = Orders.customerId
+WHERE Orders.customerId IS NULL;
