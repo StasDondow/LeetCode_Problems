45ff0494b9f9f66afb302e419d4567d7f860a498
StasDondow <125673216+StasDondow@users.noreply.github.com>
2023-12-11 12:28:55 +0200

    SQL task (Easy)

---
+++ 577_Employee_Bonus.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/employee-bonus/description/
+
+SELECT Employee.name AS name, Bonus.bonus AS bonus
+FROM Employee
+LEFT JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE Bonus.bonus < 1000
+	OR Bonus.bonus IS NULL
